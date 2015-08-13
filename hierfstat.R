3d731380ef4f9bd1b3f72fefaf6df1d7dff10c02
Shawn Goggins <shawngoggins@agro201153759.cfans.umn.edu>
2015-08-13 14:35:01 -0500

    East and West FST

--- hierfstat.R
+++ hierfstat.R
@@ -5,35 +5,35 @@ library("hierfstat")
 
 #basic.stats(data,diploid=TRUE,digits=4) #Fst
 ?basic.stats
-col1 <- as.integer(Land_6152_SNPs_AB[,1])
 nrow(Land_6152_SNPs_AB) #803
 ncol(Land_6152_SNPs_AB) #6142
-class(Land_6152_SNPs_AB[3,3]) #currently working with factors, need integers
-#AA: 1
-#BB: 2
-#AB: 3
+#AA: 11
+#BB: 22
+#AB: 12
 #is.na("NA)
 
-#Success 
-for(i in 1:ncol(Land_6152_SNPs_AB)){
-  {
-    if(i=="AA"){
-      i <- 1
-    }
-    if(i=="BB"){
-      i <- 2
-    }
-    if(i=="AB" & i=="BA"){
-      i <- 3
-    }
-    is.na("NA") 
-  }
+#to genotype
+class(Land_6152_SNPs_AB[3,4]) #needs to be character
+
+for(i in (1:ncol(Land_6152_SNPs_AB))){
+    Land_6152_SNPs_AB[,i]<-replace(Land_6152_SNPs_AB[,i],Land_6152_SNPs_AB[,i]=="AA",1)
+    Land_6152_SNPs_AB[,i]<-replace(Land_6152_SNPs_AB[,i],Land_6152_SNPs_AB[,i]=="BB",2)
+    Land_6152_SNPs_AB[,i]<-replace(Land_6152_SNPs_AB[,i],Land_6152_SNPs_AB[,i]=="AB",NA)
+    Land_6152_SNPs_AB[,i]<-replace(Land_6152_SNPs_AB[,i],Land_6152_SNPs_AB[,i]=="BA",NA)
 }
 
 View(Land_6152_SNPs_AB)
 
-col1 <- Land_6152_SNPs_AB[1,]
-basic.stats(col1, diploid=FALSE) #Error in 1 - sp2 - sHo/2/n : non-conformable arrays or Error in n/(n - 1) * (1 - sp2) : non-conformable arrays
+Land_6152_SNPs_AB$Patch <-1:nrow(Land_6152_SNPs_AB)
 
-basic.stats(Land_6152_SNPs_AB[,1],diploid=FALSE) #haploid?
-?basic.stats
+Land_genotypes <- Land_6152_SNPs_AB[,c(6154,1:6153)]
+
+View(Land_genotypes)
+
+for(i in Land_genotypes$X){
+  Land_genotypes$X<-replace(Land_genotypes$X,Land_genotypes[i,2]==row.names(EastLR),1)
+  Land_genotypes$X<-replace(Land_genotypes$X,Land_genotypes[i,2]==row.names(WestLR),0)
+}
+
+EvWvO <- basic.stats(Land_genotypes[,-1],diploid=FALSE)
+
