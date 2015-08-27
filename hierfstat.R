85661bbe9d8c8fefdef91bf52dba03948589f1c6
sgoggins42 <sgoggins@iastate.edu>
2015-08-27 11:16:52 -0500

    "SNPs by chromosome-needs to rbind"

--- hierfstat.R
+++ hierfstat.R
@@ -9,7 +9,7 @@ library("hierfstat")
 #AB: NA
 
 #import dataset
-loc.df <- `803_landraces_KML`
+loc.df <- `803_KML`
 geno.df <- Land_6152_SNPs_AB
 
 #check data type
@@ -31,18 +31,18 @@ for(i in (1:ncol(geno.df))){
   locside.df <- loc.df[,c(1,6)]
   View(locside.df)
     # merging onto the genotypes 
-  locgeno.df <- merge(x = locside.df, y=geno.df, by.x = "Accession.ID", by.y = "X")
-  head(locgeno.df[,1:3])
-  locgeno.df$Accession.ID <- 1:nrow(locgeno.df)
-  head(locgeno.df[,1:3])
+  locgenoEW.df <- merge(x = locside.df, y=geno.df, by.x = "Accession.ID", by.y = "X")
+  head(locgenoEW.df[,1:3])
+  locgenoEW.df$Accession.ID <- 1:nrow(locgenoEW.df)
+  head(locgenoEW.df[,1:3])
     #basic.stats
-  EW_basicstats <- basic.stats(locgeno.df[, -1], diploid = FALSE)
+  EW_basicstats <- basic.stats(locgenoEW.df[, -1], diploid = FALSE)
   EW_basicstats #FST = 0.0802
 
 #### Making a column for spring vs winter
   Habit.df <- barley_merged[ ,c(2,10)]
   head(Habit.df)
-  locHabit.df <- merge(x = locHabit.df, y=geno.df, by.x = "Accession.ID", by.y = "X")
+  locHabit.df <- merge(x = Habit.df, y=geno.df, by.x = "Accession.ID", by.y = "X")
   head(locHabit.df[,1:3])
   locHabit.df$Accession.ID <- 1:nrow(locHabit.df)
     # Winter:1, Spring:2
@@ -51,27 +51,92 @@ for(i in (1:ncol(geno.df))){
   SpWi_basicstats <- basic.stats(locHabit.df[, -1], diploid = FALSE)
   SpWi_basicstats #FST = 0.0225
   
-#Making a column for chr2 vs others
-  genmap <- GeneticMap_iSelect_9k
-  chr2<-subset(genmap,genmap$Chro == '2H')
-  head(chr2)
-  catchrows <- chr2[,c(1,2,7)]
-  catchSNPs <- catchrows[,1]
   
-  SNPs_head <- data.frame(x=catchSNPs)
-  tgeno.df <- t(geno.df)
-  View(tgeno.df)
+######### Makes a column for SNPs by chromosome
+  #tgeno = t(geno.df) without header - exel  
+  #genmap <- GeneticMap_iSelect_9k
+  #chr2<-subset(genmap,genmap$Chro == '2H')
+  #chr2_SNPs <- chr2[,1]
+  AllSNPs <- GeneticMap_iSelect_9k[,c("SNP","Chromosome")]
+  genoSNPs.df <- merge(x = tgeno, y=AllSNPs, by.x = "SNP.ID", by.y = "SNP") #chromosome at end
+  
+  genoSNPs.df <- genoSNPs.df[,c(1,805,2:804)] #write.csv(genoSNPs.df,file="SNPs_by_chrom.csv")
+  View(genoSNPs.df[1:5,1:5])
 
-  write.csv(tgeno.df,file="tgeno.csv")
-  ?write.csv()
-  
-  SNPs_1.df <- merge(x = tgeno, y=SNPs_head, by.x = "X", by.y = "x")
-  View(SNPs_1.df)
-  
-  
-#Making a row for 2 v 5 v 6 row inflorescence 
-  
+  chr1_geno <- subset(genoSNPs.df,Chromosome=="1")
+    n1 <- chr1_geno$SNP.ID
+    chr1_geno <- as.data.frame(t(chr1_geno[,-1]))
+    colnames(chr1_geno) <- n1
+    chr1_geno <- chr1_geno[-1,]
+    chr1_geno$chr <- 1
+    chr1_geno <-   chr1_geno[,c(ncol(chr1_geno),1:(ncol(chr1_geno)-1))] 
+    View(chr1_geno[1:5,1:5])
+    write.csv(chr1_geno,file="chr1_geno")
+    
+  chr2_geno <- subset(genoSNPs.df,Chromosome=="2")
+    n2 <- chr2_geno$SNP.ID
+    chr2_geno <- as.data.frame(t(chr2_geno[,-1]))
+    colnames(chr2_geno) <- n2
+    chr2_geno <- chr2_geno[-1,]
+    chr2_geno$chr <- 2
+    chr2_geno <-   chr2_geno[,c(ncol(chr2_geno),2:(ncol(chr2_geno)-1))] 
+    View(chr2_geno[1:5,1:5])
+    write.csv(chr2_geno,file="chr2_geno")
+    
+  chr3_geno <- subset(genoSNPs.df,Chromosome=="3")
+    n3 <- chr3_geno$SNP.ID
+    chr3_geno <- as.data.frame(t(chr3_geno[,-1]))
+    colnames(chr3_geno) <- n3
+    chr3_geno <- chr2_geno[-1,]
+    chr3_geno$chr <- 3
+    chr3_geno <-   chr3_geno[,c(ncol(chr3_geno),2:(ncol(chr3_geno)-1))] 
+    View(chr3_geno[1:5,1:5])
+    write.csv(chr3_geno,file="chr3_geno")
+    
+  chr4_geno <- subset(genoSNPs.df,Chromosome=="4")
+    n4 <- chr4_geno$SNP.ID
+    chr4_geno <- as.data.frame(t(chr4_geno[,-1]))
+    colnames(chr4_geno) <- n4
+    chr4_geno <- chr4_geno[-1,]
+    chr4_geno$chr <- 4
+    chr4_geno <-   chr4_geno[,c(ncol(chr4_geno),2:(ncol(chr4_geno)-1))] 
+    View(chr4_geno[1:5,1:5])
+    write.csv(chr4_geno,file="chr4_geno")
+    
+  chr5_geno <- subset(genoSNPs.df,Chromosome=="5")
+    n5 <- chr5_geno$SNP.ID
+    chr5_geno <- as.data.frame(t(chr5_geno[,-1]))
+    colnames(chr5_geno) <- n5
+    chr5_geno <- chr5_geno[-1,]
+    chr5_geno$chr <- 5
+    chr5_geno <-   chr5_geno[,c(ncol(chr5_geno),2:(ncol(chr5_geno)-1))] 
+    View(chr5_geno[1:5,1:5])
+    write.csv(chr5_geno,file="chr5_geno")
+    
+  chr6_geno <- subset(genoSNPs.df,Chromosome=="6")
+    n6 <- chr6_geno$SNP.ID
+    chr6_geno <- as.data.frame(t(chr6_geno[,-1]))
+    colnames(chr6_geno) <- n6
+    chr6_geno <- chr6_geno[-1,]
+    chr6_geno$chr <- 6
+    chr6_geno <-   chr6_geno[,c(ncol(chr6_geno),2:(ncol(chr6_geno)-1))] 
+    View(chr6_geno[1:5,1:5])
+    write.csv(chr6_geno,file="chr6_geno")
+    
+  chr7_geno <- subset(genoSNPs.df,Chromosome=="7")
+    n7 <- chr7_geno$SNP.ID
+    chr7_geno <- as.data.frame(t(chr7_geno[,-1]))
+    colnames(chr7_geno) <- n7
+    chr7_geno <- chr7_geno[-1,]
+    chr7_geno$chr <- 7
+    chr7_geno <-   chr7_geno[,c(ncol(chr7_geno),2:(ncol(chr7_geno)-1))] 
+    View(chr7_geno[1:5,1:5])
+    write.csv(chr7_geno,file="chr7_geno")
+
+bySNPs.df <- list.files(pattern="chr[1-7]_geno")
+bySNPs_table <- lapply(bySNPs.df,read.csv,header=TRUE)
+combinedSNPs.df <- do.call(rbind, bySNPs_table)
+View(combinedSNPs.df[450:460,1:5])
 
 
-  
-
+#genoSNPs.df$Chromosome <- ifelse(test = genoSNPs.df$Chromosome == "2H", yes="2", no="1")
