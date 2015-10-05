3ad693bbb31fe4aa9e2317d8939ba0f81e2b7790
sgoggins42 <sgoggins@iastate.edu>
2015-10-05 11:50:23 -0500

    Only hierfstat analyses

--- hierfstat.R
+++ hierfstat.R
@@ -1,7 +1,7 @@
 #install.packages("hierfstat") #depends on 'gtools' and 'ade4'
 library("hierfstat")
 
-#import dataset
+#import datasets
 `803_landraces_KML` <- read.csv("~/Desktop/Landrace_Analysis/Worked_Datasets/803_landraces_KML.csv")
 Land_6152_SNPs_AB <- read.delim("~/Desktop/Landrace_Analysis/Worked_Datasets/Land_6152_SNPs_AB.txt")
 GeneticMap_iSelect_9k <- read.delim("~/Desktop/Landrace_Analysis/Worked_Datasets/GeneticMap_iSelect_9k.txt")
@@ -19,6 +19,8 @@ for(i in (1:ncol(geno.df))){
     geno.df[,i]<-replace(geno.df[,i],geno.df[,i]=="BA",NA)
 }
 
+write.csv(geno.df, file = "geno.df")
+
 #### E or W 
     # West is 1, East is 2
   loc.df$side <- ifelse(test = loc.df$Longitude <48, yes = "1", no = "2")
@@ -30,43 +32,6 @@ for(i in (1:ncol(geno.df))){
   EW_basicstats <- basic.stats(locgenoEW.df[, -1], diploid = FALSE)
   write.csv(EW_basicstats$perloc,file="EW_basicstats.csv")
      #graph outlier SNPs
-  EW_basicstats <- read.csv("~/Desktop/EW_basicstats.csv")
-  EW_Fst <- EW_basicstats[,c("X","Fst")]
-  SNP_by_chro <- GeneticMap_iSelect_9k[,c("SNP","cm","Chromosome")]
-
-  SNP_FST.df <- merge(x = EW_Fst, y = SNP_by_chro, by.x = "X", by.y = "SNP")
-
-  with(SNP_FST.df, plot(x = jitter(Chromosome,2),y = Fst,
-                        cex = .75, col = ifelse(Fst > .5, "red", "black"), pch = ifelse(Fst > .5, 19, 21),
-                        xlab = "Chromosome", ylab = "Fst value", main = "Outlier Fst Values"))
-  abline(h=.5, lty = 6, lwd = .5, col = "red")
-    #table of outliers
-  Outliers <- subset(SNP_FST.df, Fst > .5)
-  tgeno <- read.csv("~/Desktop/Landrace_Analysis/Worked_Datasets/tgeno.csv")
-  outliers.in.samples <- merge(x = Outliers, y = tgeno, by.x = "X", by.y = "SNP.ID")
-    #2H pericentric inversion: ~67-74cm
-  chro.2.inversion <- subset(Outliers, Chromosome == 2)
-    #map of chromosome 2 outliers
-  library("maps")
-  library("maptools")
-  
-  quartz()
-  data("wrld_simpl")
-  plot(wrld_simpl,xlim=c(-30,140),ylim=c(-8,64),col="lemonchiffon2",main="Barley Landraces")
-  box()
-  
-  locgenoEW.df$side <- ifelse(test = locgenoEW.df$SCRI_RS_185513 == 2, yes = "1", no = "2")
-  SNP.ID.2 <- locgenoEW.df[,c("Accession.ID", "side")]
-  loc.df <- loc.df[,c(1:5)]
-  SNP.ID.2 <- merge(x = SNP.ID.2, y = loc.df, by.x = "Accession.ID", by.y = "Accession.ID")
-  with(SNP.ID.2, points(x = Longitude,y = Latitude, cex = .75,
-                                col = ifelse(side == "1", "red","blue")))
-  abline(v=48) #Zagaros 
-  
-  legend("bottomleft", bg = "transparent",
-         c("Zagaros Mountains","Derived Chromosome 2", "Ancestral Chromosome 2"),
-         col=c("black","blue","red"), lwd=1, lty=c(1,NA,NA),
-         pch = c(NA,21,21),cex = 0.8)
   
   #### Making a column for 2 v 6 row barley
   Rowtype.df <- barley_compare_merged[,c(2,11)]
